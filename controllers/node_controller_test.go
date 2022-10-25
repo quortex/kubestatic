f94b0655657ddc5804d78514b71c2047f233bc32
Vincent Marguerie <24724195+vincentmrg@users.noreply.github.com>
2022-10-25 16:43:00 +0200

    Kubebuilder 3.7.0 (#63)
    * set kubebuilder 3.7.0 style, upgrade dependencies and upgrade go version to 1.19
    
    * add contribution documentation
    
    * fix typo

--- controllers/node_controller_test.go
+++ controllers/node_controller_test.go
@@ -23,7 +23,7 @@ import (
 	"context"
 	"time"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/quortex/kubestatic/api/v1alpha1"
 	"github.com/quortex/kubestatic/pkg/provider"
